db429231effbe5a568109ca00083775d8e6ef36b
Allison Sousa Bahls <allison_sousa_bahls@hotmail.com>
2021-02-21 18:04:27 -0400

    create project api

---
+++ LibraryCardAPI/LibraryCardAPI/Model/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryCardAPI.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public String  Course { get; set; }
+        public int RegistrationNumber { get; set; }
+        public string Photo { get; set; }
+        public string Validate { get; set; }
+    }
+}
