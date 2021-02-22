78a2ff0083b08a448bb7ce54b5ddaaed9894b72b
Allison Sousa Bahls <allison_sousa_bahls@hotmail.com>
2021-02-21 22:22:46 -0400

    models and first migration

--- LibraryCardAPI/LibraryCardAPI/Models/User.cs
+++ LibraryCardAPI/LibraryCardAPI/Models/User.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace LibraryCardAPI.Model
+namespace LibraryCardAPI.Models
 {
-    public class User
+    public class User : IdentityUser<int>
     {
         public int FullName { get; set; }
     }
---
+++ LibraryCardAPI/LibraryCardAPI/Repository/Context/LibraryCardContext.cs
@@ -0,0 +1,19 @@
+﻿using LibraryCardAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryCardAPI.Repository.Context
+{
+    public class LibraryCardContext : IdentityDbContext
+    {
+        public LibraryCardContext(){ }
+
+        public LibraryCardContext(DbContextOptions<LibraryCardContext> options) : base(options){}
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
