b5fddd0121398eec638fd3da7b56061c7d97b477
Allison Sousa Bahls <allison_sousa_bahls@hotmail.com>
2021-02-25 22:11:17 -0400

    Implementation Paging

---
+++ LibraryCardAPI/LibraryCardAPI/Utils/PageList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryCardAPI.Utils
+{
+    public class PageList<T> : List<T>
+    {
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalResults { get; set; }
+        public string SortDirections { get; set; }
+        public int TotalPages { get; set; }
+        public bool HasPrevious => CurrentPage > 1;
+        public bool HasNext => CurrentPage < TotalResults;
+    
+        public PageList(List<T> items, int count, int pageNumer, int pageSize)
+        {
+            TotalResults = count;
+            PageSize = pageSize;
+            CurrentPage = pageNumer;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            AddRange(items);
+        }
+
+        public static PageList<T> ToPagedList(IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var count = source.Count();
+            var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+
+            return new PageList<T>(items, count, pageNumber, pageSize);
+        }
+
+    }
+}
