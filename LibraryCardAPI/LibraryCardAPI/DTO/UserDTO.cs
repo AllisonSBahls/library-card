6f297ae2430bc42ee45eaf3488453bded0c05a04
Allison Sousa Bahls <42558015+AllisonSBahls@users.noreply.github.com>
2021-03-02 17:38:26 -0400

    Update UserDTO.cs

--- LibraryCardAPI/LibraryCardAPI/DTO/UserDTO.cs
+++ LibraryCardAPI/LibraryCardAPI/DTO/UserDTO.cs
@@ -7,5 +7,10 @@ namespace LibraryCardAPI.DTO
 {
     public class UserDTO
     {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
     }
 }
