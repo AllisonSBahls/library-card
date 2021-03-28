47c2b207e12016be1ade674021d5d42ec03ba987
AllisonSBahls <allison_sousa_bahls@hotmail.com>
2021-03-28 18:38:39 -0400

    Load card for edits

--- library-card-web/src/Pages/Students/StudentForm.tsx
+++ library-card-web/src/Pages/Students/StudentForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { toast } from 'react-toastify'
 import { createStudent } from "../../Services/Students";
 import { IStudents } from "./types";
@@ -15,16 +15,14 @@ export default function StudentForm({student}: Props) {
     imagePhoto: defaultPhoto
   }
 
-  
+  const [values, setValues] = useState<IStudents>(student);
   const [image, setImage] = useState(ImageProps);
   const [name, setName] = useState('');
+  const [id, setId] = useState(0);
   const [course, setCourse]= useState('');
   const [expiration, setExpiration]= useState('');
   const [registrationNumber, setRegistrationNumber] = useState('');
   const token = localStorage.getItem("Token");
-  
-  var expire = new Date(student.validate);
-  console.log();
 
   const authorization = {
     headers: {
@@ -32,6 +30,11 @@ export default function StudentForm({student}: Props) {
     },
   };
 
+
+  useEffect(() =>{
+    if(student.id === 0) formCardDefault();
+    else loadCardStudent();
+  }, [student])
 
   const showPreview = (e: any) => {
     if (e.target.files && e.target.files[0]){
@@ -54,6 +57,36 @@ export default function StudentForm({student}: Props) {
     }
   }
 
+  async function formCardDefault(){
+    setName('');
+    setId(0);
+    setCourse('');
+    setRegistrationNumber('');
+    setImage({
+      imageFile: null,
+      imagePhoto: defaultPhoto
+    });
+    
+    var date = new Date();
+
+    setExpiration(date.toLocaleDateString("en-CA"))
+  }
+
+  async function  loadCardStudent() {
+    setName(student.name);
+    setId(student.id);
+    setCourse(student.course);
+    setRegistrationNumber(student.course);
+    setImage({
+      imageFile: null,
+      imagePhoto: student.photo
+    });
+    
+    var date = new Date(student.validate);
+
+    setExpiration(date.toLocaleDateString("en-CA"))
+  }
+
   async function insertStudent(e: React.FormEvent){
     e.preventDefault();
     try{
@@ -67,9 +100,9 @@ export default function StudentForm({student}: Props) {
     
     await createStudent(formData, authorization)
     toast.success("Estudante cadastrado");
-  }catch(err){
-    toast.error("Erro ao inserir o estudante");
-  }
+    }catch(err){
+      toast.error("Erro ao inserir o estudante");
+    }
   }
 
 
@@ -79,27 +112,27 @@ export default function StudentForm({student}: Props) {
         <form onSubmit={insertStudent} className="form-create">
           <div className="field-photo">
             <div className="field-photo-image">
-                <img className="photo" src={student.id !== 0 ? `https://localhost:5001/resources/images/${student.photo}`: image.imagePhoto}  alt="" />
+                <img className="photo" src={id !== 0 ? `https://localhost:5001/resources/images/${image.imagePhoto}`: image.imagePhoto}  alt="" />
              </div>
             <input type="file" onChange={showPreview} accept="image/*"/>
           </div>
           <div className="field-name">
             <label>Nome: </label>
-            <input type="text" value={student.name} onChange={e => setName(e.target.value)}/>
+            <input type="text" value={name} onChange={e => setName(e.target.value)}/>
           </div>
           <div className="field-course">
             <label>Curso: </label>
-            <input type="text" value={student.course} onChange={e => setCourse(e.target.value)}/>
+            <input type="text" value={course} onChange={e => setCourse(e.target.value)}/>
           </div>
 
           <div className="field-infos">
             <div className="field-code">
               <label>Código do Sistema: </label>
-              <input type="text" value={student.registrationNumber} onChange={(e) => {setRegistrationNumber(e.target.value)}}/>
+              <input type="text" value={registrationNumber} onChange={(e) => {setRegistrationNumber(e.target.value)}}/>
             </div>
             <div className="field-expirate">
               <label>Validade: </label>
-              <input type="date" value={expire.toLocaleDateString("en-CA")} onChange={e => setExpiration(e.target.value)}/>
+              <input type="date" value={expiration} onChange={e => setExpiration(e.target.value)}/>
             </div>
           </div>
           <button type="submit" className="button-save-continue">Cadastrar Estudante</button>
